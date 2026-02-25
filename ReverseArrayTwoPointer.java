95c0efc04322e0aaad35c85af89a95d7a40ef809
pallavinarsinge04 <pallavinarsinge123@gmail.com>
2026-02-25 20:09:36 +0530

    Implemented Reverse Array Using two-pointer technique (o(n) time , o(1) space)

---
+++ ReverseArrayTwoPointer.java
@@ -0,0 +1,24 @@
+public class ReverseArrayTwoPointer {
+    public static void reverse (int[] arr){
+        int left = 0;
+        int right = arr.length -1;
+
+        while (left < right){
+            int temp = arr[left];
+            arr [left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1 , 2 , 3 , 4 , 5};
+        reverse(arr);
+
+        System.out.print("Reversed Array: ");
+        for (int num : arr){
+            System.err.print(num + "");
+        }
+    }
+}
