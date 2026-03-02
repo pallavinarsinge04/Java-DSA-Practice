4347bab4dbcd2d408b875e901f50b5e3e2b47652
pallavinarsinge04 <pallavinarsinge123@gmail.com>
2026-03-02 18:20:37 +0530

    Day 8: Implemented Reverse Linked List (Iterative approach)

---
+++ Day-8/ReverseLinkedList.java
@@ -0,0 +1,42 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class ReverseLinkedList {
+
+    public static ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        return prev;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+
+        head = reverse(head);
+        printList(head);
+    }
+}
